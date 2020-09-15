b0fc4c5d847b57cadbe2af7e36ab198a2c24f6e9
keisuke inoue <inoue@sophia-s.co.jp>
2020-09-15 16:43:25 +0900

    console.log削除 lint fix

--- plugins/addClassBrowser.js
+++ plugins/addClassBrowser.js
@@ -3,12 +3,14 @@ import { UAParser } from 'ua-parser-js'
 export default () => {
   window.addEventListener('load', () => {
     const uaParser = new UAParser()
-    console.log(uaParser.getResult())
     const htmlTag = document.getElementsByTagName('html')
     const browserName = uaParser.getResult().browser.name
     const browserMajor = uaParser.getResult().browser.major
     if (browserName) htmlTag[0].classList.add(browserName.toLowerCase())
-    if (browserName && browserMajor) htmlTag[0].classList.add(browserName.toLowerCase()+browserMajor.toLowerCase())
+    if (browserName && browserMajor)
+      htmlTag[0].classList.add(
+        browserName.toLowerCase() + browserMajor.toLowerCase()
+      )
     const osName = uaParser.getResult().os.name
     if (osName)
       htmlTag[0].classList.add(osName.replace(/ /g, '-').toLowerCase())
